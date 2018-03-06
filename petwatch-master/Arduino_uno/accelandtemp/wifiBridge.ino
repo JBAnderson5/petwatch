48c64eb79a8a093ead0446f2e21be713a2f5f9cb
JBAnderson5 <35785660+JBAnderson5@users.noreply.github.com>
2018-03-06 15:46:34 -0800

    Arduino (#4)
    * add basic arduino tests
    
    * basic functionality for acceleration and temp sensors
    
    * reorganized arduino code
    
    created different folders for the uno code and wifi controller code
    
    * split code into multiple files and added data structures for temp
    
    * created a system to collect data
    
    data from the temp sensor collected every minute. average temperature of five minutes is stored. accelerometer is polled as often as possible and the total movement of five minutes is collected. Data structures support an hour of data collection
    
    * more of the same but need to fix time error
    
    Error where timing isn't working right. suspect problems with comparing unsigned longs
    
    * saturday work

---
+++ petwatch-master/Arduino_uno/accelandtemp/wifiBridge.ino
@@ -0,0 +1,39 @@
+
+void setupWifi(){
+  Wire.begin();
+  prevTrans=0;
+}
+
+void sendAccel(){
+
+  Wire.beginTransmission(8);
+  Wire.write("accel");
+
+  for(int i=0;i<accelIndex;i++){
+    Wire.write(aggMovement, sizeof(aggMovement));
+    Wire.write(&accelTime[i]);
+    aggMovement[i]=0;
+    accelTime[i]=0;
+  }
+  Wire.endTransmission();
+  accelIndex=0;
+}
+
+void sendTemp(){
+
+  Wire.beginTransmission(8);
+  Wire.write("temp");
+  for(int i=0;i<storedIndex;i++){
+    Wire.write(avgTemp[i]);
+    Wire.write(avgHum[i]);
+    Wire.write(avgHI[i]);
+    Wire.write(timeStamp[i]);
+    avgTemp[i]=0;
+    avgHum[i]=0;
+    avgHI[i]=0;
+    timeStamp[i]=0;
+  }
+  Wire.endTransmission();
+  storedIndex=0;
+}
+
